62eb7fdb8b7c9698325ede4878b922c6cf6d6d46
solar-mist <solarmistreal@gmail.com>
2023-03-11 12:51:57 +0000

    Add A20 line enabling

--- src/stage2/stage2.asm
+++ src/stage2/stage2.asm
@@ -3,8 +3,12 @@
 stage2:
     mov si, stage2_msg
     call real_mode_print
+
+    call enable_a20 ; Enable A20 line
+
     jmp $ ; Loop forever
 
 %include "real_mode_print.inc"
+%include "a20.inc"
 
 stage2_msg: db "Loaded stage 2 bootloader", 13, 10, 0
